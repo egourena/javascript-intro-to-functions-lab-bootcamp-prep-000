79f0d225462584dce85270a5fe4116cb0b4701d6
Diego Urena <diego.urenas@gmail.com>
2018-06-15 22:06:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function shout(string) { return string.toUpperCase() }
 function whisper(string) {return string.toLowerCase() }
 
 function logshout(string) {
-  console.log(${toUpperCase(string)
+  console.log('${toUpperCase(string)')
+}
   
     
 
