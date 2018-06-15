4c36183a6377c2ab702d74dbf1938a30f6e062b2
Diego Urena <diego.urenas@gmail.com>
2018-06-15 22:33:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function logShout(string) {
 
 function sayHiToGrandma (string) { if(string.toLowerCase()===string) {
 return "I can\'t hear you!" }  if(string.toUpperCase()===string) {
-return "YES INDEED!" }
+return "YES INDEED!" } 
 }
